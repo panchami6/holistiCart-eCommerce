0b68a5ec4d7ae4ce20fe309af1f51f70be795e61
panchami6 <62180655+panchami6@users.noreply.github.com>
2021-04-18 21:52:30 +0530

    updated home component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ProductListing } from "./Products";
 import { Cart } from "./Cart";
 import { Wishlist } from "./WishList";
 import NotFound from "./404";
+import Home from "./Home";
 
 export default function App() {
   
@@ -13,12 +14,13 @@ export default function App() {
     <div className="App">
       <nav className="navigation">
   
-        <Link className="nav-link" to="/"> Home </Link> ||
+        <Link className="nav-link" to="/Home"> Home </Link> ||
         <Link className="nav-link" to="/Products"> Products </Link> ||
         <Link className="nav-link" to="/Cart">Cart </Link> ||
         <Link className="nav-link" to="/WishList">WishList </Link>
       </nav>
       <Routes>
+        <Route path = "/Home" element = {<Home />} />
         <Route path="/Cart" element={<Cart />} />
         <Route path="/Products" element={<ProductListing />} />
         <Route path="/WishList" element={<Wishlist />} />
